d0ad3895cf46032fdc2c65308d736a5d1c21178d
Vishweshwaran <vishwa300499@gmail.com>
2020-03-24 16:51:29 +0530

    Action Bar and Material Design

--- app/src/main/java/com/dynamicdudes/kotlinretrofit/MainActivity.kt
+++ app/src/main/java/com/dynamicdudes/kotlinretrofit/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.dynamicdudes.kotlinretrofit
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
@@ -9,22 +11,15 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import stream.customalert.CustomAlertDialogue
-
-
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportActionBar!!.hide()
-
         val retrofit = Retrofit.Builder()
             .baseUrl("https://type.fit")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-
         val api = retrofit.create(ApiService::class.java)
-
         api.fetchAllUser().enqueue(object : Callback<List<User>>{
             override fun onFailure(call: Call<List<User>>, t: Throwable) {
                 val alert =
@@ -33,6 +28,7 @@ class MainActivity : AppCompatActivity() {
                         .setTitle("No Internet Connection")
                         .setMessage("Please check you internet connection. If it is not turned on please turn it on. ")
                         .setNegativeText("OK")
+                        .setNegativeColor(R.color.colorPrimaryDark)
                         .setOnNegativeClicked { view, dialog -> dialog.dismiss() }
                         .setDecorView(window.decorView)
                         .build()
@@ -43,6 +39,28 @@ class MainActivity : AppCompatActivity() {
                 showData(response.body()!!)
             }
         })
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.example_menu,menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.about_us -> {
+                val alert = CustomAlertDialogue.Builder(this@MainActivity)
+                .setStyle(CustomAlertDialogue.Style.DIALOGUE)
+                .setTitle("This App Was Developed By")
+                .setMessage("- R.Malini \n- R.Vishweshwaran\n- K. Magesh Babu \n(Founder of Dynamic Dudes)  ")
+                .setNegativeText("OK")
+                .setNegativeColor(R.color.redbar)
+                .setOnNegativeClicked { view, dialog -> dialog.dismiss() }
+                .setDecorView(window.decorView)
+                .build()
+            alert.show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
     private fun showData(users: List<User>) {
         recycler_view.apply{
